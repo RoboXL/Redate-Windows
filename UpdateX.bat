506d73b4ae0cc3703625b8e4315b41aa2c074dd4
RoboXL <105053104+RoboXL@users.noreply.github.com>
2023-09-15 13:06:01 -0400

    I made it a bit more personal ;>

--- UpdateX.bat
+++ UpdateX.bat
@@ -2,7 +2,7 @@
 
 echo Check for new versions of the script at https://sites.google.com/view/updatex or open "update updatex"
 echo ""
-echo Hello there, this script will update your installed apps with Winget.
+echo What's up %username%? This script will update most of your installed apps with Winget.
 echo ""
 echo Some apps might not update due to having an unknown version or it updates directly from the app.
 echo ""
@@ -23,7 +23,7 @@ if /i "%proceed%"=="y" (
     winget upgrade --all
     echo -------------------------------
 echo ""
-echo It is recommended that you restart your computer
+echo It is recommended that you restart your computer. Also %username% is a cool username
 echo ""
 ) else (
     echo ""
